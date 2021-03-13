3e285493dfac5069807407212fdf117173dca448
Vesa Jääskeläinen <dachaac@gmail.com>
2021-03-13 17:45:14 +0200

    leshan-client-demo: When requesting certificate over EST use endpoint name
    In order to transition from IEEE Secure Device Identity to more easier to
    compare Subject DN's use end point name as CN value.
    
    Signed-off-by: Vesa Jääskeläinen <dachaac@gmail.com>

--- leshan-client-demo/src/main/java/org/eclipse/leshan/client/demo/EstClientOperationsImpl.java
+++ leshan-client-demo/src/main/java/org/eclipse/leshan/client/demo/EstClientOperationsImpl.java
@@ -8,11 +8,17 @@ import org.bouncycastle.pkcs.jcajce.JcaPKCS10CertificationRequestBuilder;
 import org.eclipse.leshan.client.est.EstClientOperations;
 import org.eclipse.leshan.client.est.EstDeviceSettings;
 
+import javax.security.auth.x500.X500Principal;
 import java.security.PrivateKey;
 import java.security.cert.X509Certificate;
 
 public class EstClientOperationsImpl implements EstClientOperations {
     private EstDeviceSettings estDeviceSettings;
+    private String endpointName;
+
+    public EstClientOperationsImpl(String endpointName) {
+        this.endpointName = endpointName;
+    }
 
     @Override
     public void persistEstDeviceSettings(EstDeviceSettings estDeviceSettings) {
@@ -22,9 +28,9 @@ public class EstClientOperationsImpl implements EstClientOperations {
 
     @Override
     public byte[] generateCertificateSigningRequest(X509Certificate clientCertificate, PrivateKey privateKey) throws Exception {
-        PKCS10CertificationRequestBuilder p10Builder
-                = new JcaPKCS10CertificationRequestBuilder(
-                clientCertificate.getSubjectX500Principal(), clientCertificate.getPublicKey());
+        X500Principal subjectDN = new X500Principal("CN=" + endpointName);
+        PKCS10CertificationRequestBuilder p10Builder = new JcaPKCS10CertificationRequestBuilder(subjectDN,
+                clientCertificate.getPublicKey());
         JcaContentSignerBuilder csBuilder = new JcaContentSignerBuilder(clientCertificate.getSigAlgName());
         ContentSigner signer = csBuilder.build(privateKey);
         PKCS10CertificationRequest csr = p10Builder.build(signer);
